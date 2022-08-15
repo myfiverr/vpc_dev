efef87697a50fd44b0e685ecd2944ddd810b9a52
Somnath Musib <7885767+musibs@users.noreply.github.com>
2022-08-16 01:36:23 +0300

    Cleanup

--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 variable "region" {
   description = "The region to launch the host"
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
   description = "The CIDR block of the VPC"
+  default = "10.78.0.0/16"
 }
 
 variable "vpc_name" {
   description = "The name tag for the VPC"
+  default = "Dev VPC"
 }
 
 variable "owner" {
